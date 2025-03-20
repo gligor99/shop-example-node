9166442e1d2a2e748350b2eb91ba0cb78b1a90fa
gligor99 <lukagligorevic@gmail.com>
2025-03-20 15:20:22 +0100

    Added ORM Sequelize

--- util/database.js
+++ util/database.js
@@ -1,11 +1,14 @@
-const { Pool } = require("pg");
+const { Sequelize } = require("sequelize");
 
-const db = new Pool({
-  user: process.env.DB_USER || "postgres",
-  host: process.env.DB_HOST || "localhost", // points to postgres-db in docker-compose
-  database: process.env.DB_NAME || "shop",
-  password: process.env.DB_PASSWORD || "your_password",
-  port: process.env.DB_PORT || 5432,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || "shop",
+  process.env.DB_USER || "postgres",
+  process.env.DB_PASSWORD || "your_password",
+  {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "postgres",
+    port: process.env.DB_PORT || 5432,
+  }
+);
 
-module.exports = db;
+module.exports = sequelize;
